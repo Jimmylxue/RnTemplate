c62f0298d9fbdf70872f83df67e03713df9d5914
jimmyxuexue <1002661758@qq.com>
2025-03-27 13:55:40 +0800

    nativewind接入

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.png',
+          '.jpg',
+        ],
+        alias: {
+          '@src': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
